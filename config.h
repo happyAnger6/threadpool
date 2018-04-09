dc404f2864104c7be00da07859ba6fc7d938bc01
k8s-master <zhangxa@k8s-master.localdomain>
2018-04-09 06:44:24 -0700

    init

---
+++ config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Name of package */
+#define PACKAGE "threadpool"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "happyAnger6@163.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "threadpool"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "threadpool 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "threadpool"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
---
+++ src/test/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("hello the world\n");
+	return 0;
+}
+
